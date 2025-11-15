780593a3b377389f532b0fd6119e89d5c2de2c6d
Rodrigo Martinez <rodrigomartinez@MacBook-Pro-de-Rodrigo.local>
2025-11-15 20:30:08 -0300

    first version of calories problem in go

---
+++ 2022/go/1/calorie_counting.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"bufio"
+	"strconv"
+)
+
+func main() {
+	filePath := "calories.txt"
+	caloriesFile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+	scanner := bufio.NewScanner(caloriesFile)
+	mostCalories := 0
+	mostCaloriesElf := 1
+	currentElfCalories := 0
+	currentElf := 1
+	for scanner.Scan() {
+		currentCalories, _ := strconv.Atoi(scanner.Text())
+		currentElfCalories += currentCalories
+		if (currentElfCalories > mostCalories) {
+			mostCaloriesElf = currentElf
+			mostCalories = currentElfCalories
+		}
+		if currentCalories == 0 {
+			currentCalories = 0
+			fmt.Println("New elf")
+			currentElf += 1
+			currentElfCalories = 0
+		}
+	}
+	fmt.Printf("Most calories elf: %d  with %d calories \n", mostCaloriesElf, mostCalories)
+}
