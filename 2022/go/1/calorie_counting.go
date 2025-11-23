334bbd911300a8974434e1b89faacc239c5dbade
r0m0enc <rodrigo.martinez5@walmart.com>
2025-11-23 14:46:55 -0300

    add second half of first problem of 2022

--- 2022/go/1/calorie_counting.go
+++ 2022/go/1/calorie_counting.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"os"
+	"bufio"
 	"fmt"
 	"log"
-	"bufio"
+	"os"
+	"slices"
 	"strconv"
 )
 
 func main() {
-	filePath := "calories.txt"
+	filePath := "caloriesTopElves.txt"
 	caloriesFile, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 	}
-	mostCaloriesElf, mostCalories := getElfWithMostCalories(caloriesFile)
-	fmt.Printf("Most calories elf: %d  with %d calories \n", mostCaloriesElf, mostCalories)
+	//	mostCaloriesElf, mostCalories := getElfWithMostCalories(caloriesFile)
+	//	fmt.Printf("Most calories elf: %d  with %d calories \n", mostCaloriesElf, mostCalories)
+	totalCalories := getTopNElves(caloriesFile, 3)
+	fmt.Printf("Total calories: %d", totalCalories)
 }
-
 
 func getElfWithMostCalories(caloriesFile *os.File) (int, int) {
 	scanner := bufio.NewScanner(caloriesFile)
@@ -28,7 +30,7 @@ func getElfWithMostCalories(caloriesFile *os.File) (int, int) {
 	for scanner.Scan() {
 		currentCalories, _ := strconv.Atoi(scanner.Text())
 		currentElfCalories += currentCalories
-		if (currentElfCalories > mostCalories) {
+		if currentElfCalories > mostCalories {
 			mostCaloriesElf = currentElf
 			mostCalories = currentElfCalories
 		}
@@ -39,4 +41,42 @@ func getElfWithMostCalories(caloriesFile *os.File) (int, int) {
 		}
 	}
 	return mostCaloriesElf, mostCalories
+}
+
+func indexOf(element int, data []int) int {
+	for index, elementInSlice:= range data {
+		if elementInSlice == element {
+			return index
+		}
+	}
+	return -1
+}
+
+func getTopNElves(caloriesFile *os.File, numberOfElves int) int {
+	scanner := bufio.NewScanner(caloriesFile)
+	var topCalories []int
+	currentElfCalories := 0
+	for scanner.Scan() {
+		currentCalories, _ := strconv.Atoi(scanner.Text())
+		currentElfCalories += currentCalories
+		if currentCalories == 0 {
+			if len(topCalories) < numberOfElves {
+				topCalories = append(topCalories, currentCalories)
+			}
+
+			minimumCalories := slices.Min(topCalories)
+			if minimumCalories < currentElfCalories {
+				indexOfMin := indexOf(minimumCalories, topCalories)
+				topCalories = slices.Delete(topCalories, indexOfMin, indexOfMin+1)
+				topCalories = append(topCalories, currentElfCalories)
+			}
+			currentCalories = 0
+			currentElfCalories = 0
+		}
+	}
+	total := 0
+	for _, value := range topCalories {
+		total += value
+	}
+	return total
 }
